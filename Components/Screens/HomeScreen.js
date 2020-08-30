2ab1237deec00df41548fb3c99000cf9fd150f02
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-08-30 18:07:13 +0900

    Home Header + Nav 구현

--- Components/Screens/HomeScreen.js
+++ Components/Screens/HomeScreen.js
@@ -1,27 +1,38 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            navigation: props.navigation
+        };
+    }
+
     render() {
-        const { navigation } = this.props;
+        const { navigation } = this.state;
         return (
             <SafeAreaView style={styles.container}>
-                <Text>Home Screen</Text>
-                <Button
-                    title="Go to Idea"
-                    onPress={() => {
-                        navigation.navigate('Idea', {
-                            post: "Post Text"
-                        })
-                    }}
-                />
-                <Button
-                    title="Open Modal"
-                    onPress={() => navigation.navigate('MenuModal')}
-                />
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={this._openMenu}>
+                        <Image
+                            style={styles.btnMenu}
+                            source={require('../../assets/icon/menu.png')}
+                        />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.contentContainer}>
+                    <Text>바이러스</Text>
+                </View>
             </SafeAreaView>
         );
+    }
+
+    _openMenu = (event) => {
+        event.stopPropagation();
+        const { navigation } = this.state;
+        navigation.navigate('MenuModal');
     }
 }
 
@@ -31,7 +42,24 @@ const styles = StyleSheet.create({
         backgroundColor: '#f3f3f3',
         alignItems: 'center',
         justifyContent: 'center',
-      },
+    },
+    header: {
+        width: "100%",
+        height: "9%",
+        flexDirection: "row",
+        justifyContent: "flex-end"
+    },
+    contentContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    btnMenu: {
+        width: 25,
+        height: 25,
+        resizeMode: "contain",
+        margin: 30
+    }
 });
 
 export default HomeScreen;
