4db52bdc7b5878d9113a6d08878637cfe5890676
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-08-30 16:44:52 +0900

    디렉토리명 변경 + function -> class

---
+++ Components/Screens/IdeaScreen.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+class IdeaScreen extends React.Component {
+    render() {
+        const { navigation } = this.props;
+        return (
+            <View style={styles.container}>
+                <Text>Idea Screen</Text>
+                <Button
+                    title="Go to Idea... again"
+                    onPress={() => navigation.push('Idea')}
+                />
+                <Button
+                    title="Go to Home"
+                    onPress={() => navigation.push('Home')}
+                />
+                <Button
+                    title="Go back"
+                    onPress={() => navigation.goBack()}
+                />
+                <Button
+                    title="Go back to first screen in stack"
+                    onPress={() => navigation.popToTop()}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f3f3f3',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+});
+
+export default IdeaScreen;
