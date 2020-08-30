d14c538066431d2a08d4ba20858b6850dfec0425
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-08-31 01:18:49 +0900

    화면 구현

--- Components/Screens/WriteScreen.js
+++ Components/Screens/WriteScreen.js
@@ -1,16 +1,76 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 class WriteScreen extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            navigation: props.navigation,
+            title: "",
+            idea: ""
+        };
+    }
+    
     render() {
-        const { navigation } = this.props;
+        const { navigation, title, idea } = this.state;
+        const { word } = this.props.route.params;
+
         return (
             <SafeAreaView style={styles.container}>
-                <Text>Write Screen</Text>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={this._cancelWrite}>
+                        <Text style={[styles.text, {color: "#999"}]}>취소</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.text}>{word}</Text>
+                    <TouchableOpacity onPress={this._cancelWrite}>
+                        <Text style={[styles.text, {color: "#666"}]}>완료</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.content}>
+                    <View style={styles.inputTitleContainer}>
+                        <Text style={styles.text}>#</Text>
+                        <TextInput 
+                            style={[
+                                styles.text,
+                                styles.input
+                            ]}
+                            value={title}
+                            onChangeText={this._controlInputTitle}
+                            returnKeyType={"done"}
+                            placeholder="제목"
+                            placeholderTextColor="#999"
+                        />
+                    </View>
+                    <TextInput 
+                        style={[
+                            styles.text,
+                            {marginTop: 35}
+                        ]}
+                        value={idea}
+                        onChangeText={this._controlInputIdea}
+                        multiline={true}
+                        placeholder="아이디어를 입력하세요."
+                        placeholderTextColor="#999"
+                    />
+                </View>
             </SafeAreaView>
         );
     }
+
+    _controlInputTitle = (text) => {
+        this.setState({
+            title: text
+        });
+    };
+
+    _controlInputIdea = (text) => {
+        this.setState({
+            idea: text
+        });
+    };
 }
 
 const styles = StyleSheet.create({
@@ -19,7 +79,33 @@ const styles = StyleSheet.create({
         backgroundColor: '#f3f3f3',
         alignItems: 'center',
         justifyContent: 'center',
-      },
+    },
+    header: {
+        width: "100%",
+        height: "9%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingLeft: 30,
+        paddingRight: 30
+    },
+    content: {
+        flex: 1,
+        width: "100%",
+        padding: 30
+    },
+    inputTitleContainer: {
+        flexDirection: "row",
+        marginTop: 20
+    },
+    text: {
+        fontSize: 18,
+        fontFamily: 'Noto-Serif-KR'
+    },
+    input: {
+        marginLeft: 10,
+        width: 333
+    }
 });
 
 export default WriteScreen;
