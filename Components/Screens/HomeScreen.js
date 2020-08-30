971edf00fdff8a1f4dd6e51a6dfe1ae71bd5af5e
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-08-30 20:43:28 +0900

    setState 변경

--- Components/Screens/HomeScreen.js
+++ Components/Screens/HomeScreen.js
@@ -11,6 +11,7 @@ let customFonts = {
 class HomeScreen extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
             navigation: props.navigation,
             fontsLoaded: false,
@@ -72,7 +73,6 @@ class HomeScreen extends React.Component {
     async _loadFontsAsync() {
         await Font.loadAsync(customFonts);
         this.setState({
-            ...this.state,
             fontsLoaded: true
         })
     }
