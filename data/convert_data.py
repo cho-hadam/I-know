fff1af28f323bc4465a052306f1e24447fb33fc5
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-09-10 23:49:50 +0900

    list -> json, json 파일 저장

---
+++ data/convert_data.py
@@ -0,0 +1,23 @@
+import os
+import csv
+import json
+
+current_path = os.path.dirname(os.path.realpath(__file__))
+print(current_path)
+os.chdir(current_path)
+
+with open('./word_data.csv', 'r', encoding='utf-8') as file:
+    lines_list = csv.reader(file)
+
+    words_set = set()
+    for line in lines_list:
+        if line[1] != '명':
+            continue
+        line[0] = ''.join(i for i in line[0] if not i.isdigit())
+        words_set.add(line[0])
+
+    words_list = list(words_set)
+
+with open('./word_data.json', 'w', encoding='utf-8') as out_file:
+    words_json = json.dumps(words_list, indent=2, ensure_ascii=False)
+    out_file.write(words_json)
